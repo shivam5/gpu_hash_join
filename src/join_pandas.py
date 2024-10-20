a7643dfbed2107f1e02a18a29cc90f9d31f66bc6
James Lee <kyuyeonca@gmail.com>
2024-10-20 14:56:57 -0400

    Add support for multiple types in join_pandas - __init__ supports file path and dict - inner_join supports left and right keys for join

--- src/join_pandas.py
+++ src/join_pandas.py
@@ -2,7 +2,7 @@ from typing import List
 import pandas as pd
 
 class TableJoin:
-    def __init__(self, table_a: dict, table_b: dict):
+    def __init__(self, table_a: dict | pd.DataFrame | str, table_b: dict | pd.DataFrame | str):
         """
         Args:
             table_a: Table in dictionary implementation
@@ -20,27 +20,44 @@ class TableJoin:
         ```
         which follows how pandas DataFrames are dumped.
         """
-        self.table_a = pd.DataFrame(table_a)
-        self.table_b = pd.DataFrame(table_b)
+        if isinstance(table_a, str) and isinstance(table_b, str):
+            self.table_a = pd.read_csv(table_a)
+            self.table_b = pd.read_csv(table_b)
 
-    def inner_join(self, join_key: str, return_keys: List[str]) -> dict:
+        elif isinstance(table_a, dict) and isinstance(table_b, dict):
+            self.table_a = pd.DataFrame(table_a)
+            self.table_b = pd.DataFrame(table_b)
+
+        elif isinstance(table_a, pd.DataFrame) and isinstance(table_b, pd.DataFrame):
+            self.table_a = table_a
+            self.table_b = table_b
+        else:
+            raise TypeError
+
+    def inner_join(self, join_key: str | tuple, return_keys: List[str]) -> dict:
         """
         Args:
-            join_key: the key to perform inner join
+            join_key: the key to perform inner join, can be tuple for left and right key
             return_key: list of keys that table_c will contain
         Returns:
             table_c: resulting table of inner join(table_a, table_b)
 
-        `join_key` must be present in both `table_a` and `table_b`.
+        if single `join_key` must be present in both `table_a` and `table_b` if given as single string
 
         The equivalent SQL is to this function is:
             ```SELECT return_keys FROM table_a INNER JOIN table_b ON table_a.join_key = table_b.join_key
         """
+        if isinstance(join_key, tuple) and len(join_key) == 2:
+            left_key, right_key = join_key
+            result = self.table_a.merge(self.table_b, left_on=left_key, right_on=right_key, how='inner')
 
-        result = self.table_a.merge(self.table_b, on=join_key, how='inner')
-        result = result[return_keys]
+        elif isinstance(join_key, str):
+            result = self.table_a.merge(self.table_b, on=join_key, how='inner')
 
-        return result.to_dict()
+        else:
+            raise TypeError
+
+        return result[return_keys].to_dict()
 
     def left_join(self) -> dict:
         """
