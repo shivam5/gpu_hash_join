ef90ab8e7effce86e8b1b393e83d1e00f190b152
James Lee <kyuyeonca@gmail.com>
2024-10-22 10:22:55 -0400

    Implement matrix multiplication by numpy
    - remove unnecessary test cases

--- src/tester_functional.py
+++ src/tester_functional.py
@@ -1,9 +1,10 @@
 import unittest
 import join_pandas as jp
-import join_triton as jt
+import join_numpy as jn
+
 
 class TestJoin(unittest.TestCase):
-    def test_inner_join_simple(self):
+    def test_inner_join_simple_numpy(self):
         table_a = {
             'id': {0:1, 1:2, 2:3, 3:4},
             'name': {0:'Jiashen', 1:'Shivam', 2:'Sirish', 3:'James'}
@@ -14,18 +15,15 @@ class TestJoin(unittest.TestCase):
             'order': {0:'pizza', 1:'taco', 2:'sushi', 3:'sandwitch'}
         }
 
-        result = {
-            'id': {0:3, 1:4},
-            'name': {0:'Sirish', 1:'James'},
-            'order': {0:'pizza', 1:'taco'}
-        }
+        tablejoinmatrix = jn.TableJoinNumpy(table_a, table_b)
+        table_c = tablejoinmatrix.inner_join('id', ['id', 'name', 'order'])
 
-        tablejoin = jp.TableJoin(table_a, table_b)
-        table_c = tablejoin.inner_join('id', ['id', 'name', 'order'])
+        tablejoinpandas = jp.TableJoin(table_a, table_b)
+        result = tablejoinpandas.inner_join('id', ['id', 'name', 'order'])
 
         self.assertEqual(table_c, result)
 
-    def test_inner_join_nonunique(self):
+    def test_inner_join_nonunique_numpy(self):
         table_a = {
             'id': {0:1, 1:2, 2:3, 3:4},
             'name': {0:'Jiashen', 1:'Shivam', 2:'Sirish', 3:'James'}
@@ -36,18 +34,15 @@ class TestJoin(unittest.TestCase):
             'order': {0:'pizza', 1:'taco', 2:'sushi', 3:'sandwitch'}
         }
 
-        result = {
-            'id': {0:3, 1:4, 2:4},
-            'name': {0:'Sirish', 1:'James', 2:'James'},
-            'order': {0:'pizza', 1:'taco', 2:'sushi'}
-        }
+        tablejoinmatrix = jn.TableJoinNumpy(table_a, table_b)
+        table_c = tablejoinmatrix.inner_join('id', ['id', 'name', 'order'])
 
-        tablejoin = jp.TableJoin(table_a, table_b)
-        table_c = tablejoin.inner_join('id', ['id', 'name', 'order'])
+        tablejoinpandas = jp.TableJoin(table_a, table_b)
+        result = tablejoinpandas.inner_join('id', ['id', 'name', 'order'])
 
         self.assertEqual(table_c, result)
 
-    def test_inner_join_nonunique2(self):
+    def test_inner_join_nonunique2_numpy(self):
         table_a = {
             'id': {0:1, 1:2, 2:2, 3:3},
             'name': {0:'Jiashen', 1:'Shivam', 2:'Sirish', 3:'James'}
@@ -58,14 +53,11 @@ class TestJoin(unittest.TestCase):
             'order': {0:'pizza', 1:'taco', 2:'sushi', 3:'sandwitch'}
         }
 
-        result = {
-            'id': {0:2, 1:2, 2:3},
-            'name': {0:'Shivam', 1:'Sirish', 2:'James'},
-            'order': {0:'pizza', 1:'pizza', 2:'taco'}
-        }
+        tablejoinmatrix = jn.TableJoinNumpy(table_a, table_b)
+        table_c = tablejoinmatrix.inner_join('id', ['id', 'name', 'order'])
 
-        tablejoin = jp.TableJoin(table_a, table_b)
-        table_c = tablejoin.inner_join('id', ['id', 'name', 'order'])
+        tablejoinpandas = jp.TableJoin(table_a, table_b)
+        result = tablejoinpandas.inner_join('id', ['id', 'name', 'order'])
 
         self.assertEqual(table_c, result)
 
