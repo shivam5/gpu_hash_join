d628e8271c53a40d48eecd127fddd736983c2ded
Shivam Mittal <shivammittal2908@gmail.com>
2024-10-23 10:25:26 -0400

    Added appropriate abstractions

--- src/join_triton.py
+++ src/join_triton.py
@@ -1,86 +1,12 @@
-# Wrapper for triton matrix multplication to implement table join lives here.
-# TODO: Create a parent class for `TableJoin` and `TableJoinTriton` to abstract out common code.
-# and it should be an abstract class. 
-from typing import List
-import pandas as pd
-import utils
 from triton_matmul import matmul
 import torch
 import numpy as np
+from join_matmul import TableJoinMatmul
 
 
-class TableJoinTriton:
-    def __init__(self, table_a: dict | pd.DataFrame | str, table_b: dict | pd.DataFrame | str):
-        """
-        Args:
-            table_a: Table in dictionary implementation
-            table_b: Table in dictionary implementation
-        Returns:
-            None
+class TableJoinTriton(TableJoinMatmul):
 
-        We first use dictionaries to emulate tables. The format is:
-        ```
-        {
-            'id': {0:1, 1:2, 2:3, 3:4},
-            'name': {0:'foo', 1:'bar', 2:'baz', 3:'qux'},
-            ...
-        }
-        ```
-        which follows how pandas DataFrames are dumped.
-        """
-        if isinstance(table_a, str) and isinstance(table_b, str):
-            self.table_a = pd.read_csv(table_a)
-            self.table_b = pd.read_csv(table_b)
-
-        elif isinstance(table_a, dict) and isinstance(table_b, dict):
-            self.table_a = pd.DataFrame(table_a)
-            self.table_b = pd.DataFrame(table_b)
-
-        elif isinstance(table_a, pd.DataFrame) and isinstance(table_b, pd.DataFrame):
-            self.table_a = table_a
-            self.table_b = table_b
-        else:
-            raise TypeError
-
-    def inner_join(self, join_key: str | tuple, return_keys: List[str]) -> dict:
-        """
-        Args:
-            join_key: the key to perform inner join, can be tuple for left and right key
-            return_key: list of keys that table_c will contain
-        Returns:
-            table_c: resulting table of inner join(table_a, table_b)
-
-        if single `join_key` must be present in both `table_a` and `table_b` if given as single string
-
-        The equivalent SQL is to this function is:
-            ```SELECT return_keys FROM table_a INNER JOIN table_b ON table_a.join_key = table_b.join_key
-        """
-        if isinstance(join_key, tuple) and len(join_key) == 2:
-            raise NotImplementedError
-
-        elif isinstance(join_key, str):
-            domain = list(set(self.table_a[join_key].values).union(set(self.table_b[join_key].values)))
-
-            mat_a = utils.d2mat(self.table_a, domain)
-            mat_b = utils.d2mat(self.table_b, domain)
-
-            # Convert mat_a, mat_b (np.ndarray) to torch tensors
-            # Perform matrix multiplication
-            torch_mat_a = torch.tensor(mat_a, dtype=torch.float16, device='cuda')
-            torch_mat_b_t = torch.tensor(mat_b.T, dtype=torch.float16, device='cuda')
-            result = matmul(torch_mat_a, torch_mat_b_t).cpu().numpy()
-            ai, bj = np.nonzero(result)
-
-            result = pd.DataFrame()
-            for key in return_keys:
-                if key in self.table_a.keys():
-                    result[key] = self.table_a[key].iloc[ai].values
-                else:
-                    result[key] = self.table_b[key].iloc[bj].values
-
-            result.reset_index()
-
-        else:
-            raise TypeError
-
-        return result.to_dict()
+    def matmul(self, mat_a : np.ndarray, mat_b : np.ndarray) -> np.ndarray:
+        torch_mat_a = torch.tensor(mat_a, dtype=torch.float16, device='cuda')
+        torch_mat_b = torch.tensor(mat_b, dtype=torch.float16, device='cuda')
+        return matmul(torch_mat_a, torch_mat_b).cpu().numpy()
--- src/tester_functional.py
+++ src/tester_functional.py
@@ -3,7 +3,6 @@ import join_pandas as jp
 import join_numpy as jn
 import join_triton as jt
 
-
 class TestJoin(unittest.TestCase):
     def test_inner_join_simple_numpy(self):
         table_a = {
@@ -16,15 +15,19 @@ class TestJoin(unittest.TestCase):
             'order': {0:'pizza', 1:'taco', 2:'sushi', 3:'sandwitch'}
         }
 
-        tablejoinmatrix = jt.TableJoinTriton(table_a, table_b)
-        table_c = tablejoinmatrix.inner_join('id', ['id', 'name', 'order'])
+        table_join_triton = jt.TableJoinTriton(table_a, table_b)
+        result_triton = table_join_triton.inner_join('id', ['id', 'name', 'order'])
 
-        tablejoinpandas = jp.TableJoin(table_a, table_b)
-        result = tablejoinpandas.inner_join('id', ['id', 'name', 'order'])
+        table_join_numpy = jn.TableJoinNumpy(table_a, table_b)
+        result_numpy = table_join_numpy.inner_join('id', ['id', 'name', 'order'])
 
-        self.assertEqual(table_c, result)
+        table_join_pandas = jp.TableJoinPandas(table_a, table_b)
+        result_pandas = table_join_pandas.inner_join('id', ['id', 'name', 'order'])
 
-    def test_inner_join_nonunique_numpy(self):
+        self.assertEqual(result_triton, result_pandas)
+        self.assertEqual(result_numpy, result_pandas)
+
+    def test_inner_join_non_unique_numpy(self):
         table_a = {
             'id': {0:1, 1:2, 2:3, 3:4},
             'name': {0:'Jiashen', 1:'Shivam', 2:'Sirish', 3:'James'}
@@ -35,15 +38,19 @@ class TestJoin(unittest.TestCase):
             'order': {0:'pizza', 1:'taco', 2:'sushi', 3:'sandwitch'}
         }
 
-        tablejoinmatrix = jt.TableJoinTriton(table_a, table_b)
-        table_c = tablejoinmatrix.inner_join('id', ['id', 'name', 'order'])
+        table_join_triton = jt.TableJoinTriton(table_a, table_b)
+        result_triton = table_join_triton.inner_join('id', ['id', 'name', 'order'])
 
-        tablejoinpandas = jp.TableJoin(table_a, table_b)
-        result = tablejoinpandas.inner_join('id', ['id', 'name', 'order'])
+        table_join_numpy = jn.TableJoinNumpy(table_a, table_b)
+        result_numpy = table_join_numpy.inner_join('id', ['id', 'name', 'order'])
 
-        self.assertEqual(table_c, result)
+        table_join_pandas = jp.TableJoinPandas(table_a, table_b)
+        result_pandas = table_join_pandas.inner_join('id', ['id', 'name', 'order'])
 
-    def test_inner_join_nonunique2_numpy(self):
+        self.assertEqual(result_triton, result_pandas)
+        self.assertEqual(result_numpy, result_pandas)
+
+    def test_inner_join_non_unique2_numpy(self):
         table_a = {
             'id': {0:1, 1:2, 2:2, 3:3},
             'name': {0:'Jiashen', 1:'Shivam', 2:'Sirish', 3:'James'}
@@ -54,20 +61,24 @@ class TestJoin(unittest.TestCase):
             'order': {0:'pizza', 1:'taco', 2:'sushi', 3:'sandwitch'}
         }
 
-        tablejoinmatrix = jt.TableJoinTriton(table_a, table_b)
-        table_c = tablejoinmatrix.inner_join('id', ['id', 'name', 'order'])
+        table_join_triton = jt.TableJoinTriton(table_a, table_b)
+        result_triton = table_join_triton.inner_join('id', ['id', 'name', 'order'])
 
-        tablejoinpandas = jp.TableJoin(table_a, table_b)
-        result = tablejoinpandas.inner_join('id', ['id', 'name', 'order'])
+        table_join_numpy = jn.TableJoinNumpy(table_a, table_b)
+        result_numpy = table_join_numpy.inner_join('id', ['id', 'name', 'order'])
 
-        self.assertEqual(table_c, result)
+        table_join_pandas = jp.TableJoinPandas(table_a, table_b)
+        result_pandas = table_join_pandas.inner_join('id', ['id', 'name', 'order'])
+
+        self.assertEqual(result_triton, result_pandas)
+        self.assertEqual(result_numpy, result_pandas)
 
 def suite():
     suite = unittest.TestSuite()
     # Add tests in the specific order you want
     suite.addTest(TestJoin('test_inner_join_simple_numpy'))
-    suite.addTest(TestJoin('test_inner_join_nonunique_numpy'))
-    suite.addTest(TestJoin('test_inner_join_nonunique2_numpy'))
+    suite.addTest(TestJoin('test_inner_join_non_unique_numpy'))
+    suite.addTest(TestJoin('test_inner_join_non_unique2_numpy'))
     return suite
 
 if __name__ == '__main__':
@@ -75,9 +86,5 @@ if __name__ == '__main__':
     unittest.main()
 
     # Running in serial
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
-
-
-
-# if __name__ == "__main__":
+    # runner = unittest.TextTestRunner()
+    # runner.run(suite())
