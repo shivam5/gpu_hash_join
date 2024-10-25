d6f9d22ff09bfac65de2a623720a6ae953a90ea8
Sirish Gambhira <sgambhira6@atl1-1-01-005-5-0.pace.gatech.edu>
2024-10-24 20:14:27 -0400

    added debug support and assert condition

--- baseline/helper.h
+++ baseline/helper.h
@@ -13,7 +13,7 @@ struct Match
     uint32_t value2;
 };
 
-const uint32_t maxCapacity = 16384;
+const uint32_t maxCapacity = 128 * 1024 * 1024;
 const uint32_t t1Size = 64;
 const uint32_t t2Size = 128;
 
@@ -23,6 +23,6 @@ KeyValue* create_hashtable();
 
 void insert_hashtable(KeyValue* hashtable, const KeyValue* kvs, uint32_t num_kvs);
 
-void transfer_data(KeyValue* hostArr, KeyValue* deviceArr, uint32_t tsize);
+void print_arr_gpu(KeyValue* deviceArr, const KeyValue* kvs, uint32_t tsize);
 
 int findMatches(Match* result, KeyValue* t1_hash, const KeyValue* t2_kvs, uint32_t s1, uint32_t s2);
--- baseline/utils.cu
+++ baseline/utils.cu
@@ -1,7 +1,6 @@
 #include "stdio.h"
 #include "stdint.h"
 #include "helper.h"
-#include "vector"
 
 // 32 bit Murmur3 hash
 __device__ uint32_t hash(uint32_t k)
@@ -17,14 +16,12 @@ __device__ uint32_t hash(uint32_t k)
 // Create a hash table. For linear probing, this is just an array of KeyValues
 KeyValue* create_hashtable() 
 {
-    // Allocate memory
     KeyValue* hashtable;
     cudaMalloc(&hashtable, sizeof(KeyValue) * maxCapacity);
 
     // Initialize hash table to empty
     static_assert(kEmpty == 0xffffffff, "memset expected kEmpty=0xffffffff");
     cudaMemset(hashtable, 0xff, sizeof(KeyValue) * maxCapacity);
-
     return hashtable;
 }
 
@@ -37,7 +34,6 @@ __global__ void gpu_hashtable_insert(KeyValue* hashtable, const KeyValue* kvs, u
         uint32_t key = kvs[threadid].key;
         uint32_t value = kvs[threadid].value;
         uint32_t slot = hash(key);
-        printf("Inserting pos: %u key: %d val: %d\n", slot, key, value);
         while (true)
         {
             uint32_t prev = atomicCAS(&hashtable[slot].key, kEmpty, key);
@@ -46,7 +42,6 @@ __global__ void gpu_hashtable_insert(KeyValue* hashtable, const KeyValue* kvs, u
                 hashtable[slot].value = value;
                 return;
             }
-
             slot = (slot + 1) & (maxCapacity-1);
         }
     }
@@ -59,36 +54,87 @@ void insert_hashtable(KeyValue* pHashTable, const KeyValue* kvs, uint32_t num_kv
     cudaMalloc(&device_kvs, sizeof(KeyValue) * num_kvs);
     cudaMemcpy(device_kvs, kvs, sizeof(KeyValue) * num_kvs, cudaMemcpyHostToDevice);
 
-    // Have CUDA calculate the thread block size
-    int mingridsize;
-    int threadblocksize;
-    cudaOccupancyMaxPotentialBlockSize(&mingridsize, &threadblocksize, gpu_hashtable_insert, 0, 0);
-
     // Create events for GPU timing
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-
     cudaEventRecord(start);
 
-    // Insert all the keys into the hash table
-    int gridsize = ((uint32_t)num_kvs + threadblocksize - 1) / threadblocksize;
-    gpu_hashtable_insert<<<gridsize, threadblocksize>>>(pHashTable, device_kvs, (uint32_t)num_kvs);
-
+    int threadblocksize = std::min(1024, (int)num_kvs);
+    int gridsize = (num_kvs + threadblocksize - 1) / threadblocksize;
+    gpu_hashtable_insert<<<gridsize, threadblocksize>>>(pHashTable, device_kvs, num_kvs);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("Kernel launch failed: %s\n", cudaGetErrorString(err));
+    }
     cudaEventRecord(stop);
-
     cudaEventSynchronize(stop);
 
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
     float seconds = milliseconds / 1000.0f;
-    // printf("GPU inserted %d items in %f ms (%f million keys/second)\n", num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
+    printf("GPU inserted %d items in %f ms (%f million keys/second)\n", num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
 
     cudaFree(device_kvs);
 }
 
-void transfer_data(KeyValue* hostArr, KeyValue* deviceArr, uint32_t tsize) {
-    cudaMemcpy(hostArr, deviceArr, sizeof(KeyValue) * tsize, cudaMemcpyDeviceToHost);
+__global__ void gpu_hashtable_print(KeyValue* deviceArr, const KeyValue* kvs, uint32_t tsize) {
+    unsigned int threadid = blockIdx.x*blockDim.x + threadIdx.x;
+    if (threadid < tsize)
+    {
+        uint32_t key = kvs[threadid].key;
+        uint32_t value = kvs[threadid].value;
+        uint32_t slot = hash(key);
+        while (true)
+        {
+            if (deviceArr[slot].key != kEmpty) {
+                if (key != deviceArr[slot].key) {
+                    printf("Mismatch found: KV key: %d Hash key: %d\n", key, deviceArr[slot].key); 
+                    return;
+                }
+                else if (value != deviceArr[slot].value) {
+                    printf("Mismatch found: KV value: %u Hash value: %u\n", value, deviceArr[slot].value); 
+                    return;
+                }
+                else {
+                    printf("Match found: key: %d value: %u\n", deviceArr[slot].key, deviceArr[slot].value);
+                    return;
+                }
+            }
+            slot = (slot + 1) & (maxCapacity-1);
+        }
+    }
+}
+
+void print_arr_gpu(KeyValue* deviceArr, const KeyValue* kvs, uint32_t tsize) {
+     // Copy the keyvalues to the GPU
+    KeyValue* device_kvs;
+    cudaMalloc(&device_kvs, sizeof(KeyValue) * tsize);
+    cudaMemcpy(device_kvs, kvs, sizeof(KeyValue) * tsize, cudaMemcpyHostToDevice);
+
+    // Have CUDA calculate the thread block size
+    int threadblocksize = std::min(1024, (int)tsize);
+    int gridsize = (tsize + threadblocksize - 1) / threadblocksize;
+
+    // Create events for GPU timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+    gpu_hashtable_print<<<gridsize, threadblocksize>>>(deviceArr, device_kvs, tsize);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("Kernel launch failed: %s\n", cudaGetErrorString(err));
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    float seconds = milliseconds / 1000.0f;
+    printf("Printed GPU %d items in %f ms (%f million keys/second)\n", tsize, milliseconds, tsize / (double)seconds / 1000000.0f);
+
+    cudaFree(device_kvs);
 }
 
 __global__ void gpu_lookup(Match* rgpu, int* rid, KeyValue* t1, KeyValue* t2dev_kvs, uint32_t s1, uint32_t s2) {
@@ -118,16 +164,31 @@ int findMatches(Match* result, KeyValue* t1_hash, const KeyValue* t2_kvs, uint32
     cudaMalloc(&t2dev_kvs, sizeof(KeyValue) * s2);
     cudaMemcpy(t2dev_kvs, t2_kvs, sizeof(KeyValue) * s2, cudaMemcpyHostToDevice);
 
-    int mingridsize;
-    int threadblocksize;
-    cudaOccupancyMaxPotentialBlockSize(&mingridsize, &threadblocksize, gpu_lookup, 0, 0);
-    
+    int threadblocksize = std::min(1024, (int)s2);
     int gridsize = (maxCapacity + threadblocksize - 1) / threadblocksize;
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+
     gpu_lookup<<<gridsize, threadblocksize>>>(rgpu, rid, t1_hash, t2dev_kvs, s1, s2);
-    cudaDeviceSynchronize();
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("Kernel launch failed: %s\n", cudaGetErrorString(err));
+    }
 
     int rpos;
     cudaMemcpy(&rpos, rid, sizeof(int), cudaMemcpyDeviceToHost);
     cudaMemcpy(result, rgpu, sizeof(Match) * rpos, cudaMemcpyDeviceToHost);
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    float seconds = milliseconds / 1000.0f;
+    printf("Found %d matches in %f ms (%f million keys/second)\n", rpos, milliseconds, rpos / (double)seconds / 1000000.0f);
+    
     return rpos;
 }
