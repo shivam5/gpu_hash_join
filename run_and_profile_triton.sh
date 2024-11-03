83d83e68c7e6d303da7b897a25ff00cdd5cf1b7d
Shivam Mittal <smittal98@gatech.edu>
2024-11-03 11:39:27 -0500

    Updating profiling script to add readable mode without nsight

--- run_and_profile_triton.sh
+++ run_and_profile_triton.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 mkdir -p profiling_output/ncu_metrics
 mkdir -p profiling_output/outputs
+
+# Export variables
 export TABLE1="table1M_id10"
 export TABLE2="table1k"
-python profiler/ncu_profile.py --bin "python src/performance.py --N1 data/$TABLE1 --N2 data/$TABLE2" --output profiling_output/ncu_metrics/${TABLE1}${TABLE2}.txt > profiling_output/outputs/${TABLE1}${TABLE2}.txt
+
+# Run benchmark with both outputs
+python profiler/ncu_profile.py \
+    --bin "python src/performance.py --N1 data/$TABLE1 --N2 data/$TABLE2" \
+    --output "profiling_output/ncu_metrics/${TABLE1}_${TABLE2}" \
+    --format both \
+    > "profiling_output/outputs/${TABLE1}_${TABLE2}.txt"
