7c59c5052436b38fab9a70f84ea2e502ebfe9b40
Shivam Mittal <smittal98@gatech.edu>
2024-11-03 23:10:52 -0500

    cuda baseline profiling results

--- baseline/evaluate.py
+++ baseline/evaluate.py
@@ -5,8 +5,8 @@ output_file = ['10', '100', '1k', '10k', '100k']
 
 for ifile in input_file:
     for ofile in output_file:
-        f1 = pd.read_csv(f'../data/result_{ifile}_{ofile}.csv')
-        f2 = pd.read_csv(f'../data/pred_table1M_id{ofile}_{ifile}.csv')
+        f1 = pd.read_csv(f'data/result_{ifile}_{ofile}.csv')
+        f2 = pd.read_csv(f'data/pred_table1M_id{ofile}_{ifile}.csv')
         if not f1.equals(f2):
             print(ifile, ofile, "Tables are not identical")
         else:
