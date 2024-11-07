c5c5f747d2e0ded5d5a2c32363e493839374f167
James Lee <kyuyeonca@gmail.com>
2024-11-07 13:33:57 -0500

    modify profile script and upload new data

--- run_and_profile_triton.sh
+++ run_and_profile_triton.sh
@@ -5,13 +5,13 @@ mkdir -p profiling_output/outputs
 
 # Define arrays of tables
 TABLE1_ARRAY=("table1M_id10" "table1M_id100" "table1M_id1k" "table1M_id10k" "table1M_id100k")
-TABLE2_ARRAY=("table1k" "table10k")
+TABLE2_ARRAY=("table1k")
 
 # Iterate over all combinations
 for TABLE1 in "${TABLE1_ARRAY[@]}"; do
     for TABLE2 in "${TABLE2_ARRAY[@]}"; do
         echo "Processing ${TABLE1} with ${TABLE2}..."
-        
+
         # Export variables
         export TABLE1
         export TABLE2
@@ -22,7 +22,7 @@ for TABLE1 in "${TABLE1_ARRAY[@]}"; do
             --output "profiling_output/ncu_metrics/${TABLE1}_${TABLE2}" \
             --format both \
             > "profiling_output/outputs/${TABLE1}_${TABLE2}.txt"
-            
+
         echo "Completed ${TABLE1} with ${TABLE2}"
         echo "----------------------------------------"
     done
